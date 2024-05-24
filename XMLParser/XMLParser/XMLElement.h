043b16ee251bbdcae25ba5c9c5eeb0f854a82a0f
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2024-05-24 11:02:06 +0300

    .

--- XMLParser/XMLParser/XMLElement.h
+++ XMLParser/XMLParser/XMLElement.h
@@ -5,13 +5,33 @@
 
 
 class IXMLElement {
+protected:
+	virtual void setParent(IXMLElement* parent) = 0;
+
 public:
+	virtual const MyString& getId() const = 0;
+
 	virtual IXMLElement* clone() const = 0;
 
 	virtual bool hasAttribute(const MyString& name) const = 0;
 
 	virtual const MyString& getAttribute(const MyString& name) const = 0;
 
+	virtual void addAttribute(const MyString& name, const MyString& value) = 0;
 
+	virtual void setAttribute(const MyString& name, const MyString& value) = 0;
 
+	virtual IXMLElement* getParent() = 0;
+
+	virtual const IXMLElement* getParent() const = 0;
+
+	virtual IXMLElement* childAt() = 0;
+
+	virtual const IXMLElement* childAt() const = 0;
+
+	virtual void addChild(IXMLElement* elem) = 0;
+
+	virtual void insertChild(size_t at, IXMLElement* elem) = 0;
+
+	virtual ~IXMLElement() = default;
 };
