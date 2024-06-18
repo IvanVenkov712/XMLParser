75505eb6d2ecfa21080f5ea169012eaf16bb52d2
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2024-06-18 17:58:46 +0300

    Added logic to StringView

--- XMLParser/XMLParser/StringView.cpp
+++ XMLParser/XMLParser/StringView.cpp
@@ -24,6 +24,99 @@ StringView StringView::substr(size_t from, size_t length) const
 	return StringView(_begin + from, _begin + from + length);
 }
 
+int StringView::findFirst(char ch) const
+{
+	for (const char* i = _begin; i < _end; ++i) {
+		if (*i == ch) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findLast(char ch) const
+{
+	for (const char* i = _end - 1; i >= _begin; --i) {
+		if (*i == ch) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findFirst(const StringView& substr) const
+{
+	size_t substrLen = substr.length();
+	if (length() < substrLen) {
+		return -1;
+	}
+	for (const char* i = _begin; i < _end - substrLen; ++i) {
+		if (strncmp(i, substr._begin, substrLen) == 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findLast(const StringView& substr) const
+{
+	size_t substrLen = substr.length();
+	if (length() < substrLen) {
+		return -1;
+	}
+	for (const char* i = _end - substrLen - 1; i >= _begin; --i) {
+		if (strncmp(i, substr._begin, substrLen) == 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findFirstOf(const StringView& chars) const
+{
+	for (const char* i = _begin; i < _end; ++i) {
+		if (chars.findFirst(*i) >= 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findLastOf(const StringView& chars) const
+{
+	for (const char* i = _end - 1; i < _begin; --i) {
+		if (chars.findFirst(*i) >= 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findFirstNotOf(const StringView& chars) const
+{
+	for (const char* i = _begin; i < _end; ++i) {
+		if (chars.findFirst(*i) < 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+int StringView::findLastNotOf(const StringView& chars) const
+{
+	for (const char* i = _end - 1; i < _begin; --i) {
+		if (chars.findFirst(*i) < 0) {
+			return i - _begin;
+		}
+	}
+	return -1;
+}
+
+StringView StringView::trim() const
+{
+	return substr(findFirstNotOf(wSpaces), findLastOf(wSpaces));
+}
+
 std::ostream& operator<<(std::ostream& os, const StringView& strView)
 {
 	const char* iter = strView._begin;
@@ -34,4 +127,65 @@ std::ostream& operator<<(std::ostream& os, const StringView& strView)
 		iter++;
 	}
 	return os;
+}
+
+
+bool operator==(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() != rhs.length()) {
+		return false;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) == 0;
+}
+
+bool operator!=(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() != rhs.length()) {
+		return true;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) != 0;
+}
+
+bool operator<(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() > rhs.length()) {
+		return false;
+	}
+	if (lhs.length() < rhs.length()) {
+		return true;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) < 0;
+}
+
+bool operator<=(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() > rhs.length()) {
+		return false;
+	}
+	if (lhs.length() < rhs.length()) {
+		return true;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) <= 0;
+}
+
+bool operator>(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() > rhs.length()) {
+		return true;
+	}
+	if (lhs.length() < rhs.length()) {
+		return false;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) > 0;
+}
+
+bool operator>=(const StringView& lhs, const StringView& rhs)
+{
+	if (lhs.length() > rhs.length()) {
+		return true;
+	}
+	if (lhs.length() < rhs.length()) {
+		return false;
+	}
+	return std::strncmp(lhs._begin, rhs._begin, lhs.length()) >= 0;
 }
--- XMLParser/XMLParser/StringView.h
+++ XMLParser/XMLParser/StringView.h
@@ -3,6 +3,8 @@
 #include "MyString.h"
 class StringView
 {
+	static constexpr const char* wSpaces = " \t\n\r\v\f";
+
 	const char* _begin;
 	const char* _end; //1 element after the final char;
 
@@ -15,6 +17,37 @@ public:
 	char operator[](size_t ind) const;
 
 	StringView substr(size_t from, size_t length) const;
+
+	int findFirst(char ch) const;
+
+	int findLast(char ch) const;
+
+	int findFirst(const StringView& substr) const;
+
+	int findLast(const StringView& substr) const;
+
+	int findFirstOf(const StringView& chars) const;
+
+	int findLastOf(const StringView& chars) const;
+
+	int findFirstNotOf(const StringView& chars) const;
+
+	int findLastNotOf(const StringView& chars) const;
+
+	StringView trim() const;
+
+	friend bool operator==(const StringView& lhs, const StringView& rhs);
+
+	friend bool operator!=(const StringView& lhs, const StringView& rhs);
+
+	friend bool operator<(const StringView& lhs, const StringView& rhs);
+
+	friend bool operator<=(const StringView& lhs, const StringView& rhs);
+
+	friend bool operator>(const StringView& lhs, const StringView& rhs);
+
+	friend bool operator>=(const StringView& lhs, const StringView& rhs);
+
 	friend std::ostream& operator<<(std::ostream&, const StringView& strView);
 
 };
